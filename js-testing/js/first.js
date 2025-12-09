f610bd821ac13ea24b9d8b7af71aebc52e93cbec
Maxim Vinogradov <seeeverylook@gmail.com>
2025-12-09 22:13:19 +0300

    feat(core): add initial JS entry file and base HTML

---
+++ js-testing/js/first.js
@@ -0,0 +1,31 @@
+const NOTIFICATION = "«Я JavaScript!».";
+
+let admin;
+let authorName = "Макс";
+
+admin = authorName;
+
+// Иная интерпретация
+
+// let admin, name;
+// name = "Джон";
+
+// admin = name;
+
+// alert(admin)
+
+let userName = "Тот самый";
+let greetings = `Привет, ${userName}`;
+let place = prompt("Где он?", "");
+let isHe = confirm("Ты, это он?");
+let myString = "моя" + " " + "строка";
+
+alert(NOTIFICATION);
+alert(admin);
+alert(greetings);
+alert(`Какой ужас, он там - ${place}`);
+alert(isHe);
+alert(myString);
+
+console.log(9007199254740991 + 1);
+console.log(9007199254740991 + 2);
